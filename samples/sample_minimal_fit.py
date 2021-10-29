0469b336cb1e834ee7fa9d50b9dc1e38762f913a
Ossi Myllymäki <omyllymaki@gmail.com>
2021-10-29 18:11:44 +0300

    Update minimal fit sample

--- samples/sample_minimal_fit.py
+++ samples/sample_minimal_fit.py
@@ -3,28 +3,32 @@ from functools import partial
 
 import numpy as np
 
-from src.gauss_newton import GaussNewton
+from src.gradient_descent import GradientDescent
 
 logging.basicConfig(level=logging.INFO)
+
+np.random.seed(21)
 
 
 def f_eval(x, param):
     return param[0] * x + param[1]
 
 
-def f_err(param, x, y):
+def f_cost(param, x, y):
     y_estimate = f_eval(x, param)
-    return y_estimate - y
+    errors = y_estimate - y
+    return np.mean(errors ** 2)
 
 
 def main():
     x = np.arange(1, 100)
     param_true = np.array([1.0, 2.5])
     y = f_eval(x, param_true)
-    optimizer = GaussNewton(f_err=partial(f_err, x=x, y=y))
+    f_step = lambda k: (0, 1e-3)
+    optimizer = GradientDescent(f_cost=partial(f_cost, x=x, y=y), f_step=f_step)
     param, costs, _ = optimizer.run(np.random.randn(2))
     print(f"Param: {param}")
-    print(f"Costs: {costs}")
+    print(f"Cost: {np.min(costs)}")
 
 
 if __name__ == "__main__":
