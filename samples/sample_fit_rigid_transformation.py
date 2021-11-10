02688e088dd673c8c64503d93eeccd0e555406ae
Ossi Myllymäki <omyllymaki@gmail.com>
2021-11-10 17:40:38 +0200

    Update fit rigid transformation sample

--- samples/sample_fit_rigid_transformation.py
+++ samples/sample_fit_rigid_transformation.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-from src.gradient_descent import GradientDescent
-from src.termination import check_n_iter
+from src.levenberg_marquardt import LevenbergMarquardt
+from src.termination import check_n_iter, check_absolute_cost_diff
 from src.utils import mse
 
 logging.basicConfig(level=logging.INFO)
@@ -43,29 +43,35 @@ def f_eval(xyz, coeff):
     return transform(t, xyz)
 
 
-def f_err(source, target):
+def calculate_distances(source, target):
     diff = source - target
     return np.linalg.norm(diff, axis=1)
 
 
 def f_cost(param, source, target):
     source_transformed = f_eval(source, param)
-    errors = f_err(source_transformed, target)
+    errors = calculate_distances(source_transformed, target)
     return mse(errors)
 
 
-def main():
-    for k in range(16):
-        target = np.random.randn(50, 3)
-        param_true = np.random.randn(6)
-        source = f_eval(target, param_true) + NOISE * np.random.randn(50, 3)
+def f_err(param, source, target):
+    source_transformed = f_eval(source, param)
+    return calculate_distances(source_transformed, target)
 
-        init_guess = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        f_step = lambda _: (0, 0.1)
-        termination_checks = partial(check_n_iter, threshold=100)
-        optimizer = GradientDescent(termination_checks=termination_checks,
-                                    f_cost=partial(f_cost, source=source, target=target),
-                                    f_step=f_step)
+
+def main():
+    for k in range(9):
+        source = np.random.randn(50, 3)
+        param_true = np.random.randn(6)
+        target = f_eval(source, param_true) + NOISE * np.random.randn(50, 3)
+
+        init_guess = np.zeros(6)
+        termination_checks = (
+            partial(check_n_iter, threshold=500),
+            partial(check_absolute_cost_diff, threshold=1e-9)
+        )
+        fe = partial(f_err, source=source, target=target)
+        optimizer = LevenbergMarquardt(f_err=fe, termination_checks=termination_checks)
         t1 = time.time()
         param, costs, _ = optimizer.run(init_guess)
         t2 = time.time()
@@ -73,8 +79,12 @@ def main():
         t = coeff_to_transform_matrix(param)
         source_transformed = transform(t, source)
 
-        residual = f_err(source_transformed, target)
-        rmse = np.sqrt(np.sum(residual ** 2))
+        print(f"Test {k}")
+        print(f"True param: {param_true}")
+        print(f"Estimated param: {param}")
+
+        distances = calculate_distances(source_transformed, target)
+        mean_distance = np.mean(distances)
 
         plt.figure(1)
         plt.subplot(2, 2, 1)
@@ -92,10 +102,10 @@ def main():
         plt.plot(costs, "-o")
 
         plt.figure(2)
-        plt.subplot(4, 4, k + 1)
+        plt.subplot(3, 3, k + 1)
         plt.plot(source_transformed[:, 0], source_transformed[:, 1], "b.", alpha=0.5)
         plt.plot(target[:, 0], target[:, 1], "r.", alpha=0.5)
-        plt.title(f"{rmse:0.2f} | {duration_ms:0.1f} ms")
+        plt.title(f"{mean_distance:0.2f} | {duration_ms:0.1f} ms")
 
         plt.pause(0.1)
 
